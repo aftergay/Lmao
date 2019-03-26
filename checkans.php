27c2b697b335996e7787fb72418e58eeedb3b075
thundersdn <sdnthunder@hotmail.com>
2019-03-26 14:43:57 -0300

    Initial

---
+++ checkans.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="/stylesheets/arisachan.css">
+	<link rel="stylesheet" href="/stylesheets/intro.css">
+	<title>Ran Astley</title>
+</head>
+<body>
+<video autoplay>
+  <source src="AfterRolled.webm" type="video/webm">
+  Your browser does not support the video tag.
+</video>
+</body>
+</html>
---
+++ question.php
@@ -0,0 +1,28 @@
+<?php
+$qst = array("Q_Arisa","Q_Saaya","Q_Aya","Q_Yukina","Q_Rinko","Q_Tae","Q_Kokoro","Q_Kanon","Q_Rimi","Q_Tomoe","Q_Michelle","Q_Hina","Q_Himari","Q_Hagumi","Q_Kasumi","Q_Kaoru","Q_Sayo","Q_Eve","Q_Ran");
+$fld = $qst[array_rand($qst)];
+$name = substr($fld,2);
+?>
+
+<html>
+<head>
+	<link rel="stylesheet" href="/stylesheets/arisachan.css">
+	<link rel="stylesheet" href="/stylesheets/intro.css">
+	<title>Answer Wisely</title>
+</head>
+<body>
+	<div class="question"> 
+		<img class="center-half" src="/intro/<?php echo $fld;?>/question.jpg"/>
+		<form action="checkans.php" method="POST">
+			<input class="q1" name="<?php echo $name?>_A" type="image" src="/intro/<?php echo $fld;?>/q0.jpg"/>
+			<input class="q2" name="<?php echo $name?>_B" type="image" src="/intro/<?php echo $fld;?>/q1.jpg"/>
+			<input class="q3" name="<?php echo $name?>_C" type="image" src="/intro/<?php echo $fld;?>/q2.jpg"/>
+			<input class="q4" name="<?php echo $name?>_D" type="image" src="/intro/<?php echo $fld;?>/q3.jpg"/>
+		</form>
+
+	</div>
+	
+
+</body>
+
+</html>
